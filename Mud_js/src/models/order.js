4a162e924f44532c69e1a320c4a7f075988ee364
github username <github email address>
2017-12-30 18:58:58 -0500

    refactored Order Class's attributes. Edited order serializer on the back end

--- Mud_js/src/models/order.js
+++ Mud_js/src/models/order.js
@@ -3,8 +3,6 @@ class Order {
     this.id = orderObj.id,
     this.client = orderObj.client,
     this.date = orderObj.date,
-    this.drinks = orderObj.drinks,
-    this.drink_ids = orderObj.drink_ids
     this.drink_orders = orderObj.drink_orders.map(drinkOrderObj => new DrinkOrder(drinkOrderObj)),
     Order.all.push(this)
   }
--- Mud_server/app/serializers/order_serializer.rb
+++ Mud_server/app/serializers/order_serializer.rb
@@ -1,6 +1,4 @@
 class OrderSerializer < ActiveModel::Serializer
   attributes :id, :client, :date
   has_many :drink_orders
-  has_many :drinks
-  has_many :drink_ids
 end
